47741b57fa8712f9a9dd0f378bfc4b13c59affec
Strategic King Godwin <strategicimpacttechagency001@gmail.com>
2026-01-03 23:09:54 +0100

    Add Day 1 Python project

---
+++ day1.py
@@ -0,0 +1,10 @@
+# Day 1: First Python Code
+name = "Strategic King Godwin"
+goal = "Build world-class technology"
+today = "Day 1"
+
+print("Hello World!")
+print("My name is", name)
+print("My goal is to", goal)
+print("Today is", today)
+print("I am starting my journey to becoming a tech builder 🚀")
